1c80aeab2631691c39f336dc0a83dced04fc19d5
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2020-04-16 13:58:09 +0300

    merge geo spatial into 2.8 (https://github.com/OHDSI/WebAPI/issues/1445)

---
+++ js/settings.js
@@ -0,0 +1,10 @@
+const settingsObject = {
+	paths: {
+        "leaflet": "../node_modules/leaflet/dist/leaflet",
+	},
+	cssPaths: {
+        "leaflet": "css!../node_modules/leaflet/dist/leaflet.css"
+	},
+	localRefs: {
+	},
+};
